778d644be1bb7803569b15b651c1ec7936dd67d2
clarkwu <superwunc@qq.com>
2024-06-18 02:52:41 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -24955,17 +24955,48 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
 const core = __importStar(__nccwpck_require__(2186));
 const node_fs_1 = __importDefault(__nccwpck_require__(7561));
+// Recursive function to get files
+function getFiles(dir, files = []) {
+    // Get an array of all files and directories in the passed directory using fs.readdirSync
+    const fileList = node_fs_1.default.readdirSync(dir);
+    // Create the full path of the file/directory by concatenating the passed directory and file/directory name
+    for (const file of fileList) {
+        const name = `${dir}/${file}`;
+        // Check if the current file/directory is a directory using fs.statSync
+        if (node_fs_1.default.statSync(name).isDirectory()) {
+            // If it is a directory, recursively call the getFiles function with the directory path and the files array
+            getFiles(name, files);
+        }
+        else {
+            // If it is a file, push the full path to the files array
+            files.push(name);
+        }
+    }
+    return files;
+}
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
     try {
-        console.log('core');
-        const www = node_fs_1.default.existsSync('dist');
-        console.log('dist', www);
-        const www2 = node_fs_1.default.existsSync('src');
-        console.log('src', www2);
+        const files = getFiles('src');
+        for (let i = 0, l = files.length; i < l; i++) {
+            const content = node_fs_1.default.readFileSync(files[i], 'utf8');
+            const regex = /(0x[0-9a-zA-Z]{40})/gm;
+            let m;
+            const contractAddress = [];
+            while ((m = regex.exec(content)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === regex.lastIndex) {
+                    regex.lastIndex++;
+                }
+                contractAddress.push(m[0]);
+            }
+            if (contractAddress.length > 0) {
+                console.log(files[i], contractAddress.length);
+            }
+        }
     }
     catch (error) {
         // Fail the workflow run if an error occurs
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,49 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+
 import fs from 'node:fs'
+
+// Recursive function to get files
+function getFiles(dir: string, files: string[] = []) {
+  // Get an array of all files and directories in the passed directory using fs.readdirSync
+  const fileList = fs.readdirSync(dir)
+  // Create the full path of the file/directory by concatenating the passed directory and file/directory name
+  for (const file of fileList) {
+    const name = `${dir}/${file}`
+    // Check if the current file/directory is a directory using fs.statSync
+    if (fs.statSync(name).isDirectory()) {
+      // If it is a directory, recursively call the getFiles function with the directory path and the files array
+      getFiles(name, files)
+    } else {
+      // If it is a file, push the full path to the files array
+      files.push(name)
+    }
+  }
+  return files
+}
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    console.log('core')
-    const www = fs.existsSync('dist')
-    console.log('dist', www)
-    const www2 = fs.existsSync('src')
-    console.log('src', www2)
+    const files = getFiles('src')
+
+    for (let i = 0, l = files.length; i < l; i++) {
+      const content: string = fs.readFileSync(files[i], 'utf8')
+      const regex = /(0x[0-9a-zA-Z]{40})/gm
+      let m
+      const contractAddress = []
+      while ((m = regex.exec(content)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+          regex.lastIndex++
+        }
+        contractAddress.push(m[0])
+      }
+      if (contractAddress.length > 0) {
+        console.log(files[i], contractAddress.length)
+      }
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
